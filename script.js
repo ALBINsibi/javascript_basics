ada0a46c08c57a1e3e7092be5c760c7cb7908665
ALBINsibi <albinsibi1000@gmail.com>
2024-06-29 17:48:23 +0530

    Data type config

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("hello world")
+var a=10;
+console.log(a);
+let name="john"
+console.log(name);
+const pi=3.14;
+console.log(pi);
+
+let valuetwo=2;
+let valueone="2";
+console.log(valuetwo+parseInt(valueone));
+const namee="JOHN"
+const age=20;
+const iscool=true;
+console.log(typeof namee)
+
+
